b56f3293a222f00b6822f2a30b2ebaeaf4ccac5a
PowerLi <pccr10001@gmail.com>
2025-06-22 20:53:30 +0800

    update esphome py

--- components/tesla_ble_vehicle/__init__.py
+++ components/tesla_ble_vehicle/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client, binary_sensor
+from esphome.components import ble_client, binary_sensor, text_sensor
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@yoziru"]
@@ -11,12 +11,13 @@ TeslaBLEVehicle = tesla_ble_vehicle_ns.class_(
     "TeslaBLEVehicle", cg.PollingComponent, ble_client.BLEClientNode
 )
 
-AUTO_LOAD = ["binary_sensor"]
+AUTO_LOAD = ["binary_sensor", "text_sensor"]
 CONF_VIN = "vin"
 CONF_IS_ASLEEP = "is_asleep"
 CONF_IS_UNLOCKED = "is_unlocked"
 CONF_IS_USER_PRESENT = "is_user_present"
 CONF_IS_CHARGE_FLAP_OPEN = "is_charge_flap_open"
+CONF_SHIFT_STATE = "shift_state"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -35,6 +36,9 @@ CONFIG_SCHEMA = (
             ).extend(),
             cv.Optional(CONF_IS_CHARGE_FLAP_OPEN): binary_sensor.binary_sensor_schema(
                 icon="mdi:ev-plug-tesla", device_class=binary_sensor.DEVICE_CLASS_DOOR
+            ).extend(),
+            cv.Optional(CONF_SHIFT_STATE): text_sensor.text_sensor_schema(
+                icon="mdi:car-shift-pattern"
             ).extend(),
         }
     )
@@ -70,3 +74,8 @@ async def to_code(config):
         conf = config[CONF_IS_CHARGE_FLAP_OPEN]
         bs = await binary_sensor.new_binary_sensor(conf)
         cg.add(var.set_binary_sensor_is_charge_flap_open(bs))
+
+    if CONF_SHIFT_STATE in config:
+        conf = config[CONF_SHIFT_STATE]
+        ts = await text_sensor.new_text_sensor(conf)
+        cg.add(var.set_text_sensor_shift_state(ts))
